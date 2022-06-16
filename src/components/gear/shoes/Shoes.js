aaf16a75c76bb4b80d9447c0d767c48e48515a65
Matthew Gammon <mgammon91@gmail.com>
2022-06-16 01:14:56 -0400

    create component to render a select menu of all shoes in the game

---
+++ src/components/gear/shoes/Shoes.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import {baseUrl} from '../../../utils/api';
+
+export default function Shoes() {
+    const [shoes, setShoes] = useState(null);
+    const [shoesError, setShoesError] = useState(null);
+
+    useEffect(() => {
+        async function loadShoes() {
+            try {
+                setShoesError(null);
+                const response = await fetch(`${baseUrl}/shoes`);
+                const data = await response.json();
+                setShoes(data);
+            } catch (error) {
+                setShoesError(error);
+            }
+        }
+
+        loadShoes();
+    }, []);
+
+    return (
+        <>
+            <div className="shoes">
+                <label htmlFor="shoes-select">Shoes</label>
+                <select name="shoes" id="shoes-select">
+                    {
+                        shoes?.map((shoe, id) => (
+                            <option value={shoe.shoeName} key={shoe.shoeId}>{shoe.shoeName}</option>
+                        ))
+                    }
+                </select>
+            </div>
+        </>
+    )
+}
