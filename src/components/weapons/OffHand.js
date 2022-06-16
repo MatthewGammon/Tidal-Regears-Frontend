448c64da7f8144cf87aedc224755bf6f0afcf91c
Matthew Gammon <mgammon91@gmail.com>
2022-06-16 00:31:00 -0400

    create component to render a select menu of all offhand weapons

---
+++ src/components/weapons/OffHand.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import {baseUrl} from '../../utils/api';
+
+export default function OffHand() {
+    const [offHands, setOffHands] = useState(null);
+    const [offHandsError, setOffHandsError] = useState(null);
+
+    useEffect(() => {
+        async function loadOffHands() {
+            try {
+                setOffHandsError(null);
+                const response = await fetch(`${baseUrl}/offHand`);
+                const data = await response.json();
+                setOffHands(data);
+            } catch (error) {
+                setOffHandsError(error);
+            }
+        }
+
+        loadOffHands();
+    }, []);
+
+    return (
+        <>
+            <div className="offHands">
+                <label htmlFor="offHand-select">OffHand:</label>
+                <select name="offHand" id="offHand-select">
+                    {
+                        offHands?.map((offHand, id) => (
+                            <option value={offHand.offHandName} key={offHand.offHandId}>{offHand.offHandName}</option>
+                        ))
+                    }
+                </select>
+            </div>
+        </>
+    )
+}
