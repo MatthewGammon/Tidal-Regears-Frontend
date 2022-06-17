e159c7e904bc0cf0b3ad0d4c0b6f1ccb93c04908
Matthew Gammon <mgammon91@gmail.com>
2022-06-16 23:01:55 -0400

    create routes to /home and /createBuild. import Nav and Home components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import {BrowserRouter, Routes, Route, Link, Navigate} from 'react-router-dom';
+import Nav from '../src/components/layout/Nav';
+import Home from '../src/components/layout/Home';
 import CreateBuild from '../src/components/builds/CreateBuild';
 
 function App() {
     return (
-        <div className="App">
-            <CreateBuild />
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <Nav/>
+                <Routes>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/" element={<Navigate replace to="/home"/>}/>
+                    <Route path="/createBuild" element={<CreateBuild/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
