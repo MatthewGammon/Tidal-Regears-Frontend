329eaf726e69973628b6ee85c56a3d7b5ca30f21
Matthew Gammon <mgammon91@gmail.com>
2022-06-21 11:01:53 -0400

    add Route for /builds to render ListBuilds component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, Link, Navigate} from 'react-router-dom';
-import Nav from '../src/components/layout/Nav';
-import Home from '../src/components/layout/Home';
-import CreateBuild from '../src/components/builds/CreateBuild';
+import Nav from '../src/components/layout/nav/Nav';
+import Home from '../src/components/layout/home/Home';
+import CreateBuild from '../src/components/builds/createABuild/CreateABuild';
+import ListBuilds from '../src/components/builds/listBuilds/ListBuilds';
 
 function App() {
     return (
@@ -13,6 +14,8 @@ function App() {
                     <Route path="/home" element={<Home/>}/>
                     <Route path="/" element={<Navigate replace to="/home"/>}/>
                     <Route path="/createBuild" element={<CreateBuild/>}/>
+                    <Route path="/builds" element={<ListBuilds/>}/>
+
                 </Routes>
             </div>
         </BrowserRouter>
