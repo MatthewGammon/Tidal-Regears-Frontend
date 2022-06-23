705adb23a8dbaa5d7679f7f86bc2c9f429ae3369
Matthew Gammon <mgammon91@gmail.com>
2022-06-23 03:20:45 -0400

    implement AbortController in useEffect. Map buildsList data to build divs displaying relevant info for each build

--- src/components/builds/listBuilds/ListBuilds.js
+++ src/components/builds/listBuilds/ListBuilds.js
@@ -1,28 +1,63 @@
 import React, {useState, useEffect} from 'react';
 import {baseUrl} from '../../../utils/api';
+import './ListBuilds.css';
 
 export default function ListBuilds() {
     const [buildsList, setBuildsList] = useState(null);
     const [fetchError, setFetchError] = useState(null);
 
     useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
         const loadBuilds = async () => {
-            setFetchError(null);
             try {
-                const response = await fetch(`${baseUrl}/builds`);
+                const response = await fetch(`${baseUrl}/builds`, {signal});
                 const data = await response.json();
                 setBuildsList(data);
             } catch (error) {
-                setFetchError(error);
+                if (error.name !== 'AbortError') {
+                    setFetchError(null);
+                }
             }
         }
         loadBuilds();
+
+        return () => {
+            controller.abort();
+            setFetchError(null);
+        };
     }, []);
 
     return (
-        <div className="test">
-            {JSON.stringify(buildsList)}
-        </div>
+        <>
+            <main className="builds-list">
+                <div className="build-header">
+                    <h2>Currently Approved ZVZ Builds</h2>
+                </div>
+                {
+                    buildsList?.map((build, id) => (
+                        <div className="build" key={build.buildId}>
+                            <h3 className="build-name">Build Name: {build.buildName}</h3>
+                            <h4 className="build-role">Role: {build.buildRole}</h4>
+                            <h4 className="min-ip">Minimum IP: {build.minimumIp}</h4>
+                            <h4 className="min-tier">Minimum Tier Equivalent: {build.minimumTier}</h4>
+                            <br/>
+                            <div className="main-hand">Main Hand: {build.mainHand}</div>
+                            <div className="off-hand">Off Hand: {build.offHand}</div>
+                            <div className="head-gear">Head: {build.headGear}</div>
+                            <div className="chest-gear">Chest: {build.chestGear}</div>
+                            <div className="shoes">Shoes: {build.shoes}</div>
+                            <div className="cape">Cape: {build.cape}</div>
+                            <div className="food">Food: {build.food}</div>
+                            <div className="potion">Potion: {build.potion}</div>
+                            <div className="mount">Mount: {build.mount}</div>
+                        </div>
+
+                    ))
+                }
+
+            </main>
+        </>
     )
 
 }
