29425e6a592f2b33c5afa713b8c65f6df2db8558
Matthew Gammon <mgammon91@gmail.com>
2022-06-25 06:56:23 -0400

    update to import image rather than putting a relative path in the src attribute

--- src/errors/NotFound.js
+++ src/errors/NotFound.js
@@ -1,11 +1,12 @@
 import './NotFound.css'
+import albionLogo from '../assets/images/albion-logo.png';
 
 export default function NotFound() {
     return (
         <div className="not-found-error">
             <h1>404: Page Not Found!</h1>
             <p>This page either does not exist or is currently unavailable.</p>
-            <img src='../assets/images/albion-logo.png' alt="albion online logo" />
+            <img src={albionLogo} alt="albion online logo" />
         </div>
     );
 }
