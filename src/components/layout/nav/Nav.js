f0858ecf0cc9be1146720488d8b4cf53c51da319
Matthew Gammon <mgammon91@gmail.com>
2022-06-26 12:29:01 -0400

    add conditional rendering for links that only approved users should be able to see/interact with

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import AuthService from '.././../../services/auth.service';
 import './Nav.css';
 
 export default function Nav() {
+    const currentUser = AuthService.getCurrentUser();
+    const isAdmin = currentUser?.roles.includes("ROLE_ADMIN");
+    const isUser = currentUser?.roles.includes("ROLE_USER");
+    const isAdminOrUser = currentUser;
+
     function displayMenu() {
         const element = document.getElementById('myTopnav');
 
@@ -13,31 +19,52 @@ export default function Nav() {
     }
 
     return (
-        <>
             <div className="topnav" id="myTopnav">
                 <a href="/home" className="active">
                     Home
                 </a>
-                <a href="/regears">Regear Requests</a>
-                <a href="/builds">View Builds</a>
-                <a href="/builds/new">Create a Build</a>
-                <a href="/requirements">Requirements</a>
+
+                {isAdminOrUser && (
+                        <a href="/regears">
+                            Regear Requests
+                        </a>
+                    )}
+                {isAdminOrUser && (
+                        <a href="/builds">
+                            View Builds</a>
+                    )}
+
+                {isAdmin && (
+                        <a href="/builds/new">
+                            Create a Build</a>
+                    )}
+                <a href="/requirements">
+                    Requirements
+                </a>
+
+                {currentUser && (
+                    <a className="user-name">{currentUser.username}</a>
+                )}
+
+                {!currentUser && (
+                        <a className="login-link" href="/login">
+                            Login</a>
+                    )}
+
+                {!currentUser && (
+                        <a className="register-link" href="/register">
+                            Register</a>
+                    )}
+
+
+                {currentUser && (
+                        <button className="logout-button btn" onClick={() => AuthService.logout()}>
+                            Logout</button>
+                    )}
 
                 <button className="icon" onClick={() => displayMenu()}>
                     <i className="fa fa-bars"></i>
                 </button>
             </div>
-
-            <div className="header">
-                <div className="header-text">
-                    <h1 className="header-primary">
-                        Tidal Regear Management System [TRMS]
-                    </h1>
-                    <p className="header-lead">
-                        View and Manage Builds and Requests instantly!
-                    </p>
-                </div>
-            </div>
-        </>
     )
 }
