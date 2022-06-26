13e466044f3bb962eb5df341fa2e57142f53e0d2
Matthew Gammon <mgammon91@gmail.com>
2022-06-26 12:25:30 -0400

    update useEffect and load functions. Extract handleApprove, handleReject functions, and regears-table data for use in a separate component

--- src/components/regears/Regears.js
+++ src/components/regears/Regears.js
@@ -1,106 +1,62 @@
 import React, {useState, useEffect} from 'react';
-import {baseUrl, updateStatus} from '../../utils/regearsService';
-import {useNavigate} from 'react-router-dom';
+import {baseUrl, updateStatus} from '../../services/regearsService';
+import UserService from '../../services/user.service';
+import authHeader from '../../services/auth-header';
+import AuthService from '../../services/auth.service';
+import ErrorAlert from '../../errors/ErrorAlert';
 import './Regears.css';
+import RegearsTable from './RegearsTable';
+
+const currentUser = AuthService.getCurrentUser();
+const isAdmin = currentUser?.roles.includes("ROLE_ADMIN");
 
 export default function Regears() {
     const [regears, setRegears] = useState(null);
     const [fetchError, setFetchError] = useState(null);
 
-    const navigate = useNavigate();
 
     useEffect(() => {
-        const controller = new AbortController();
-        const signal = controller.signal;
-
-        const loadRegears = async () => {
+        const getRegears = async () => {
+            const url = `${baseUrl}/regears`;
+            const options = {
+                method: 'GET',
+                headers: authHeader(),
+            };
             try {
-                const respone = await fetch(`${baseUrl}/regears`, {signal});
-                const data = await respone.json();
+                const response = await fetch(url, options);
+                const data = await response.json();
+                if (response.status >= 400 && response.status < 600) {
+                    console.log(response)
+                    throw new Error(`${response.status == 401 ? "Unauthorized" : response.status == 403 ? "Forbidden" : "Oops"}`);
+                }
                 setRegears(data);
             } catch (error) {
-                if (error.name !== 'AbortError') {
-                    setFetchError(null);
-                }
+                setFetchError(error)
             }
         }
-        loadRegears();
+        getRegears();
 
         return () => {
             setFetchError(null);
-            controller.abort();
         };
     }, []);
 
-    const handleApprove = async (regearId, regear) => {
-        regear.status = "approved";
-        await updateStatus(regearId, regear);
-        window.alert("Regear Request Approved!")
-        navigate("/regears");
-        // call update with "approved", regearId and update the status in db for given regear
-    }
 
-    const handleReject = async (regearId, regear) => {
-        if (
-            window.confirm("Are you sure you want to reject this request? This cannot be undone.")
-        ) {
-            regear.status = "rejected";
-            await updateStatus(regearId, regear);
-            navigate("/regears")
-
-        }
-    }
-    const pending = regears?.filter((regear) => regear.status === "pending");
-    // only display regears with a status of pending. only display buttons for pending regears.
-
+    
     return (
-        <>
             <main className="regears-list">
+                {
+                    fetchError && (
+                        <ErrorAlert error={fetchError}/>
+                    )
+                }
+
                 <div className="regears-header">
                     <h2>Current Pending Regears</h2>
                 </div>
 
-                <table className="regears-table">
-                    <tbody>
-                    <tr className="regears-table-headers">
-                        <th>Regear ID</th>
-                        <th>Status</th>
-                        <th>Player Name</th>
-                        <th>Guild</th>
-                        <th>IP</th>
-                        <th>Main Hand</th>
-                        <th>Head</th>
-                        <th>Chest</th>
-                        <th>Shoes</th>
-                        <th>Time of Death</th>
-                        <th>Event ID</th>
-                        <th>Approve/Reject</th>
-                    </tr>
-                    {
-                        pending?.map((regear) => (
-                            <tr className="regear" key={regear.regearId}>
-                                <td>{regear.regearId}</td>
-                                <td>{regear.status}</td>
-                                <td>{regear.characterName}</td>
-                                <td>{regear.guildName}</td>
-                                <td>{regear.itemPower}</td>
-                                <td>{`${regear.mainTier} ${regear.mainHand}`}</td>
-                                <td>{`${regear.headTier} ${regear.headGear}`}</td>
-                                <td>{`${regear.chestTier} ${regear.chestGear}`}</td>
-                                <td>{`${regear.shoesTier} ${regear.shoes}`}</td>
-                                <td>{regear.timeOfDeath}</td>
-                                <td>{regear.eventId}</td>
-                                <td>
-                                    <button onClick={() => handleApprove(regear.regearId, regear)}>Approve</button>
-                                    <br/>
-                                    <button onClick={() => handleReject(regear.regearId, regear)}>Reject</button>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                    </tbody>
-                </table>
+                <RegearsTable regearData={regears}/>
             </main>
-        </>
+
     )
 }
