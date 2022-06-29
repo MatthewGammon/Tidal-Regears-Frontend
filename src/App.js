fbfa6776445947402106787b080efc8fc75909e9
Matthew Gammon <mgammon91@gmail.com>
2022-06-29 01:29:35 -0400

    import AuthService to check if a user is logged in and authorized. wrap components that require authorization in ProtectedRoutes component and pass in the auth level as props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route,  Navigate} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Nav from '../src/components/layout/nav/Nav';
 import Home from '../src/components/layout/home/Home';
 import CreateBuild from '../src/components/builds/createABuild/CreateABuild';
@@ -10,25 +10,48 @@ import NotFound from '../src/errors/NotFound';
 import Login from '../src/components/login/Login';
 import Register from '../src/components/register/Register';
 import Header from '../src/components/layout/Header.js';
+import ProtectedRoute from "./components/ProtectedRoute";
+import AuthService from "./services/auth.service";
 
 function App() {
+    const currentUser = AuthService.getCurrentUser();
+    const isAdmin = currentUser?.roles.includes("ROLE_ADMIN");
+    const isAdminOrUser = currentUser;
+
     return (
         <BrowserRouter>
             <div className="app">
                 <Nav/>
-                <Header />
+                <Header/>
 
                 <Routes>
                     <Route path="*" element={<NotFound/>}/>
                     <Route path="/home" element={<Home/>}/>
                     <Route path="/" element={<Navigate replace to="/home"/>}/>
-                    <Route path="/regears" element={<Regears/>}/>
-                    <Route path="/builds" element={<ListBuilds/>}/>
-                    <Route path="/builds/new" element={<CreateBuild/>}/>
-                    <Route path="/builds/:buildId/edit" element={<CreateBuild/>}/>
                     <Route path="/requirements" element={<Requirements/>}/>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/register" element={<Register/>}/>
+
+                    <Route path="/regears" element={<ProtectedRoute user={isAdminOrUser}>
+                        <Regears/>
+                    </ProtectedRoute>
+                    }
+                    />
+                    <Route path="/builds" element={<ProtectedRoute user={isAdminOrUser}>
+                        <ListBuilds/>
+                    </ProtectedRoute>
+                    }
+                    />
+                    <Route path="/builds/new" element={<ProtectedRoute user={isAdmin}>
+                        <CreateBuild/>
+                    </ProtectedRoute>
+                    }
+                    />
+                    <Route path="/builds/:buildId/edit" element={<ProtectedRoute user={isAdmin}>
+                        <CreateBuild/>
+                    </ProtectedRoute>
+                    }
+                    />
                 </Routes>
             </div>
         </BrowserRouter>
