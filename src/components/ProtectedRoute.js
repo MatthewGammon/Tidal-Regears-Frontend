4ca79e10d7c2d66724668bccab50c72a09ff26b9
Matthew Gammon <mgammon91@gmail.com>
2022-06-29 01:28:01 -0400

    create component that checks if a user is authorized, and if so, renders child component taken in as props

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route, Navigate} from 'react-router-dom';
+
+export default function ProtectedRoute({user, children}) {
+
+    if (!user) {
+        return <Navigate to="/login" replace/>;
+    }
+
+    return children;
+}
